da9ec6b86e24a49d2f1da731008dfb4d5b87d50a
RizgiyUP <96582112+izgiy@users.noreply.github.com>
2022-11-13 23:24:02 +0500

    Create javascript.js

---
+++ javascript/javascript.js
@@ -0,0 +1,63 @@
+var ball = document.getElementById('ball');
+
+ball.onmousedown = function(e) {
+
+  var coords = getCoords(ball);
+  var shiftX = e.pageX - coords.left;
+  var shiftY = e.pageY - coords.top;
+
+  ball.style.position = 'absolute';
+  document.body.appendChild(ball);
+  moveAt(e);
+
+  ball.style.zIndex = 1000; // над другими элементами
+
+  function moveAt(e) {
+    ball.style.left = e.pageX - shiftX + 'px';
+    ball.style.top = e.pageY - shiftY + 'px';
+  }
+
+  document.onmousemove = function(e) {
+    moveAt(e);
+  };
+
+  ball.onmouseup = function() {
+    document.onmousemove = null;
+    ball.onmouseup = null;
+  };
+
+}
+
+ball.ondragstart = function() {
+  return false;
+};
+
+function getCoords(elem) {   // кроме IE8-
+  var box = elem.getBoundingClientRect();
+  return {
+    top: box.top + pageYOffset,
+    left: box.left + pageXOffset
+  };
+}
+document.addEventListener('DOMContentLoaded', () => {
+
+    const followCursor = () => { // объявляем функцию followCursor
+      const el = document.querySelector('.follow-cursor') // ищем элемент, который будет следовать за курсором
+  
+      window.addEventListener('mousemove', e => { // при движении курсора
+        const target = e.target // определяем, где находится курсор
+        if (!target) return
+  
+        if (target.closest('a')) { // если курсор наведён на ссылку
+          el.classList.add('follow-cursor_active') // элементу добавляем активный класс
+        } else { // иначе
+          el.classList.remove('follow-cursor_active') // удаляем активный класс
+        }
+  
+        el.style.left = e.pageX + 'px' // задаём элементу позиционирование слева
+        el.style.top = e.pageY + 'px' // задаём элементу позиционирование сверху
+      })
+    }
+  
+    followCursor() // вызываем функцию followCursor
+  })
